3d090cdf37bb54a4f13f180a7c7a6e1fa28363a0
hemakandregula-123 <hema@svtek.co.in>
2024-12-12 00:02:47 +0530

    Round_Robin_Arbiter_training_project

--- hema/RRA_Top.sv
+++ hema/RRA_Top.sv
@@ -38,82 +38,93 @@ module RRA_Top #(
         .req_i(req_i),
 		  .timeoutperiod_i(timeout_period),
 		  .gnt_o(rra_gnt)
-  // RRA module drives internal rra_gnt
+                                          // RRA module drives internal rra_gnt
     );
 
-    // Register write/read logic
-    always_ff @(posedge Pclk_i or negedge PResetn_i) begin
-        if (!PResetn_i) begin
-            PRData_o <= 32'h0;  // Clear the read data during reset
-        end else if (PSel_i) begin
-            if (PWrite_i) begin
-                // Write operation: Store data into the registers at specified address
+                                          // Register write/read logic
+    always_ff @(posedge Pclk_i or negedge PResetn_i) 
+	   begin
+        if (!PResetn_i) 
+		    begin
+            PRData_o <= 32'h0;            // Clear the read data during reset
+          end 
+		  else if (PSel_i)
+		    begin
+            if (PWrite_i) 
+				  begin
+                                                             // Write operation: Store data into the registers at specified address
                 case (PAddr_i)
-                    ARB_CTRL_ADDR: begin
+                    ARB_CTRL_ADDR: 
+						  begin
                         timeout_period <= PWData_i[7:4];
                         enable <= PWData_i[3:0];
                     end
-                    default: begin
+                    default:
+						  begin
                         timeout_period <= 4'h0;
                         enable <= 4'h0;
                     end
-                endcase
-            end else begin
-                // Read operation: Output data from registers at specified address
+                 endcase
+               end 
+			else
+		  	 begin
+                                                              // Read operation: Output data from registers at specified address
                 case (PAddr_i)
                     ARB_CTRL_ADDR: PRData_o <= ARB_CTRL;      // Read from ARB_CTRL
-                    ARB_STATUS_ADDR: PRData_o <= ARB_STATUS; // Read from ARB_STATUS
-                    default: PRData_o <= 32'h0;              // Default read value for invalid address
+                    ARB_STATUS_ADDR: PRData_o <= ARB_STATUS;  // Read from ARB_STATUS
+                    default: PRData_o <= 32'h0;               // Default read value for invalid address
                 endcase
-            end
+          end
         end
     end
-
-   
-
-
-    // Generate ARB_STATUS register
-    always_comb
-	 begin
-        if (!PResetn_i) begin
-            ARB_STATUS = 32'h0;
-            pending_requests = 8'h0;
-            current_grant = 8'h0;
-        end else begin
-            // Count pending requests
+                                           // Generate ARB_STATUS register
+    always_comb 
+	    begin
+         if (!PResetn_i) 
+			   begin
+             ARB_STATUS = 32'h0;
+             pending_requests = 8'h0;
+             current_grant = 8'h0;
+            end 
+			else
+		    	begin
+                                          // Count pending requests
             pending_requests = 0;
-            for (int i = 0; i < NUM_REQUESTS; i++) begin
+            for (int i = 0; i < NUM_REQUESTS; i++) 
+				 begin
                 if (req_i[i])
                     pending_requests += 1 ;
-            end
-				     pending_requests = pending_requests-8'h1; 
+             end
+				        pending_requests = pending_requests-8'h1; 
 
 
-            // Find the current grant index
+                                         // Find the current grant index
             current_grant = 8'h0;
-            for (int i = 0; i < NUM_REQUESTS; i++) begin
-                if (rra_gnt[i]) begin
-                     current_grant = (i + 1) % 256; // Mask to 8 bits
+            for (int i = 0; i < NUM_REQUESTS; i++) 
+				    begin
+                  if (rra_gnt[i]) 
+						 begin
+                     current_grant = (i + 1) % 256; 
                      break;
+                   end
                 end
-            end
 
-            // Update ARB_STATUS with 32-bit concatenation
+                                        // Update ARB_STATUS with 32-bit concatenation
             ARB_STATUS = {16'b0, current_grant, pending_requests[7:0]};
         end
     end
 
-    // Update ARB_CTRL dynamically
+                                       // Update ARB_CTRL dynamically
     always_comb begin
         ARB_CTRL = {16'b0, timeout_period, enable};
     end
-	 always_comb// @(posedge Pclk_i) 
+	 always_comb 
 	 begin
         
             if (&enable) 
-                gnt_o = rra_gnt;  // Forward the grants when enabled
+                gnt_o = rra_gnt;       // Forward the grants when enabled
             else 
-                gnt_o = 0; // Disable grant signals if arbiter is disabled
+                gnt_o = 0;             // Disable grant signals if arbiter is disabled
         end 
 	 
 endmodule
